9ef5a5c12bf5d6981f4b97a71a31d26e1fa7c0e8
Gardiner Nardini <gardiner.nardini@gmail.com>
2018-06-22 19:44:40 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,11 +14,11 @@
 
 
 def speak_to_grandma(string)
-  if string.upcase != string
-    return "HUH?! SPEAK UP, SONNY!"
-  elsif string.upcase == string
-    return "NO, NOT SINCE 1938!"
-  elsif string == "I LOVE YOU GRANDMA!"
+  if string == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  elsif string.upcase != string
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif string.upcase == string 
+    return "NO, NOT SINCE 1938!"
   end
 end
